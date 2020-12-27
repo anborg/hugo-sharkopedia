a31223109a1f7601232dfce0cb2bf1888b415e38
anborg <dev@gmail.com>
2020-12-27 02:34:21 -0500

    netlify

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
 set HOST_NAME=%COMPUTERNAME%
+::set HUGO_ENV=production
+set HUGO_ENV=development
 %cd%\hugo server --bind=0.0.0.0 --port=8083 --baseURL=http://localhost:8083/
 :: %cd%\hugo server --bind=0.0.0.0 --port=8083 --baseURL=http://%HOST_NAME%:8083/
